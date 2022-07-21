2f4f2ea2d388c8bae759362894e4da75d72d0064
paosua86 <paolasuarezb1986@gmail.com>
2022-07-21 13:12:23 -0500

    task

---
+++ _printf.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * list - checks if there is a valid format specifier
+ * @format: possible format specifier
+ * Return: pointer to valid function or NULL
+ */
+static int (*list(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+	{"c", print_c},
+	{"s", print_s},
+	{"i", print_i},
+	{"d", print_i},
+	{"b", print_b},
+	{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - prints
+ * @format: list of argument passed to the function
+ *	Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list listcomp;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(listcomp, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = list(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(listcomp);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(listcomp);
+	return (count);
+}
